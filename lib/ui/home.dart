1b5a82de1f7612a73a2209414367b2765d518637
Itamir Barroca Filho <itamirbarrocafilho@Air-de-Itamir.box>
2021-07-15 19:25:55 -0300

    ajustes

---
+++ lib/ui/home.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'result.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  void _resetCampos() {
+    pesoController = TextEditingController();
+    alturaController = TextEditingController();
+    _formKey.currentState.reset();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _resetCampos,
+          )
+        ], //<Widget>[]
+      ), // app bar
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person, size: 120, color: Colors.blueAccent),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.blueAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+                controller: pesoController,
+                validator: (value) {
+                  if (value.isEmpty) return "Insira seu peso!";
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (m)",
+                    labelStyle: TextStyle(color: Colors.blueAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+                controller: alturaController,
+                validator: (value) {
+                  if (value.isEmpty) return "Insira sua altura!";
+                },
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                      height: 50.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            _calcular();
+                          }
+                        },
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            textStyle: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold)),
+                      ))),
+            ], //<widget>[]
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _calcular() {
+    String _texto = "";
+    String _imagem = "";
+
+    double peso = double.parse(pesoController.text);
+    double altura = double.parse(alturaController.text);
+
+    double imc = peso / (altura * altura);
+    //debugPrint("Peso ${peso} e altura ${altura}");
+    //debugPrint("$imc");
+    if (imc < 18.6) {
+      _texto = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/thin.png";
+    } else if (imc >= 18.6 && imc < 24.9) {
+      _texto = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/shape.png";
+    } else if (imc >= 24.9 && imc < 29.9) {
+      _texto = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 29.9 && imc < 34.9) {
+      _texto = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 34.9 && imc < 39.9) {
+      _texto = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 40) {
+      _texto = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    }
+
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => Result(_imagem, _texto)));
+  }
+}
